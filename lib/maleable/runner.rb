c271d6cf7fc7d4693fb0ca6b53e8fe43e36e1689
Cyril Mougel <cyril.mougel@gmail.com>
2011-01-16 16:10:45 +0100

    add method to do

--- lib/maleable/runner.rb
+++ lib/maleable/runner.rb
@@ -2,6 +2,8 @@ module Maleable
   class Runner
 
     def self.run(directory)
+      update_from_remote(directory)
+      push_update_in_local(directory)
       # Seems only return an hash not 3 variables
       # example : {:changed=>[], :removed=>["./ok"], :added=>[]}
       Vigilo::Watcher.watch(directory) do |changed, added, deleted|
@@ -9,6 +11,17 @@ module Maleable
         Maleable::Action.removed(changed[:removed])
         Maleable::Action.added(changed[:added])
       end
+    end
+
+    ##
+    # Check all data from gridfs and download it in local
+    #
+    def self.update_from_remote(directory)
+    end
+
+    ##
+    # Check news of update in locale and push it on gridfs
+    def self.push_update_in_local(directory)
     end
 
   end
